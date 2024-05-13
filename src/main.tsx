5dba79d89fccb7965dc2aa4ac4a33c63ba02069b
Race Malin <racemalin@protonmail.com>
2024-05-13 10:09:26 -0700

    Added react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import theme from './theme.ts';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
