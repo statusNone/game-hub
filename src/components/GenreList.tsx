ce33a248f9e0b90ff817bd43d2ed908f57e098cf
Race Malin <racemalin@protonmail.com>
2024-05-13 12:52:22 -0700

    Refactor: simplify game query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -12,9 +12,9 @@ import getCroppedImageUrl from '../services/image-url';
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
-const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenreId, onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -36,7 +36,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
               <Button
                 whiteSpace="normal"
                 textAlign="left"
-                fontWeight={selectedGenre?.id === genre.id ? 'bold' : 'normal'}
+                fontWeight={selectedGenreId === genre.id ? 'bold' : 'normal'}
                 onClick={() => onSelectGenre(genre)}
                 fontSize="lg"
                 variant="link"
