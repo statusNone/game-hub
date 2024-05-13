fc05eca7d30e5518047b897009d114e6f9212529
Race Malin <racemalin@protonmail.com>
2024-05-13 12:02:31 -0700

    Implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,7 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery } from 'react-query';
 import { GameQuery } from '../App';
 import { Platform } from './usePlatforms';
-import APIClient from '../services/api-client';
+import APIClient, { FetchResponse } from '../services/api-client';
 
 const apiClient = new APIClient<Game>('/games');
 
@@ -15,17 +15,21 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parent_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
   });
 
 export default useGames;
