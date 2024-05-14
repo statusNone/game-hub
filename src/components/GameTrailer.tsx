3d3c0ecda059399b40263531b9bcd678f601a7e8
Race Malin <racemalin@protonmail.com>
2024-05-14 15:43:39 -0700

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,20 @@
+import useTrailers from '../hooks/useTrailers';
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  const first = data?.results[0];
+
+  return first ? (
+    <video poster={first.preview} src={first.data[480]} controls />
+  ) : null;
+};
+
+export default GameTrailer;
