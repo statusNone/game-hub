cb28b3845547d91ec766f8a9dd8d8abeeb471a81
Race Malin <racemalin@protonmail.com>
2024-05-14 16:43:01 -0700

    Tweaked game detail page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router-dom';
 import useGame from '../hooks/useGame';
-import { Heading, Spinner } from '@chakra-ui/react';
+import { GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
 import GameTrailer from '../components/GameTrailer';
@@ -14,13 +14,17 @@ const GameDetailPage = () => {
 
   if (error || !game) throw error;
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText children={game.description_raw} />
-      <GameAttributes game={game} />
-      <GameScreenShots gameId={game.id} />
-      <GameTrailer gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText children={game.description_raw} />
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameScreenShots gameId={game.id} />
+        <GameTrailer gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
